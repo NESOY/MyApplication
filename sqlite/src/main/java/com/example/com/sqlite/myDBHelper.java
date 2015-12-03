4108e2f720c4328f1b5eef3888167e26b574c7f8
jinny3770 <jinny3770@naver.com>
2015-12-03 11:47:30 +0900

    sqlite

---
+++ sqlite/src/main/java/com/example/com/sqlite/myDBHelper.java
@@ -0,0 +1,23 @@
+package com.example.com.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+class myDBHelper extends SQLiteOpenHelper {
+    public myDBHelper(Context context) {
+
+        super(context, "groupDB", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE groupTBL (gName CHAR(20) PRIMARY KEY, gNumber INTEGER);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS groupTBL");
+        onCreate(db);
+    }
+}
